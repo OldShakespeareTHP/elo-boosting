b0387e7079b081e27565480b39c86fe1617c259d
ForejyTHP <versailles.panacea@gmail.com>
2020-03-17 21:27:17 +0100

    Changed environments files for production

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,19 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Allow url generation inside of emails
+config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+config.action_mailer.perform_deliveries = false
+
+
+ActionMailer::Base.smtp_settings = {
+  :user_name => ENV['SENDGRID_LOGIN'],
+  :password => ENV['SENDGRID_PWD'],
+  :domain => 'monsite.fr',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,9 +35,6 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  # Allow url generation inside of emails
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
