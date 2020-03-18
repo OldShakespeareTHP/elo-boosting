9d4dbbc1c7847b7c94024a4ca38d2a1240c3e1b9
Louis <louis.lesur@protonmail.com>
2020-03-18 13:04:57 +0100

    Forgot password ? page Done

--- db/schema.rb
+++ db/schema.rb
@@ -38,7 +38,6 @@ ActiveRecord::Schema.define(version: 2020_03_18_111129) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "username", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -51,7 +50,6 @@ ActiveRecord::Schema.define(version: 2020_03_18_111129) do
     t.string "role", default: "customer", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "boosts", "riot_accounts"
