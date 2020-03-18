a4bdd4bd6e3a4af8fb8aa1f3d8969b5ca4831587
DevPomeis <devpomeis@protonmail.com>
2020-03-18 17:28:29 +0100

    created route, erb and implemented placeholder faqé

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "static_pages#index"
+  get "/faq", to: "static_pages#faq"
   resources :charges
   resources :boosts, only: [:new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
