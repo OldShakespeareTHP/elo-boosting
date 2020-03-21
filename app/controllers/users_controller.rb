b9ee4ccd0c3a42b9324bb133932bfb7e45a9f543
DevPomeis <devpomeis@protonmail.com>
2020-03-21 02:36:55 +0100

    Fixed security issue on riot account access through url

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,6 @@ class UsersController < ApplicationController
   before_action :check_user
   def show
     @user = User.find(params[:id])
+    redirect_to current_user unless current_user.admin? || @user == current_user
   end
 end
